cea4dd0b2638848e0a083cfe74f8501384c439a1
Vladyslav <m4gdark@gmail.com>
2021-03-07 22:09:24 +0300

    First initialization

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// == FUNCTIONS ==
+
+function s(el) {
+    return document.querySelector(el);
+}
+
+function sA(el) {
+    return document.querySelectorAll(el);
+}
+
+
+// == COMPONENTS == 
+
+
+
+// == MAIN CODE ==
+
+
