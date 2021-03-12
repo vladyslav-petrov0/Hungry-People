4665a23113ab50e5af73ad5991f06043a17584bc
Vladyslav <m4gdark@gmail.com>
2021-03-12 21:58:09 +0300

    Added optimized, responsible wave-effect btn

--- assets/js/components/wave-effect.js
+++ assets/js/components/wave-effect.js
@@ -1,75 +1,134 @@
 const btnWave = () => {
     const waveTransition = parsF(styles(qSel('.wave-effect--template')).transitionDuration) * 1000;
-    const btnItem = qSel('.btn--transparent');
+    const btns = qSelA('.btn--waved');
 
-    btnItem.addEventListener('mousedown', (e) => {
-        const wave = document.createElement('span');
-        wave.classList.add('wave-effect');
-        btnItem.append(wave);
+    for (let btnItem of btns) {
+        setWaveToBtn(btnItem);
+    };
 
-        const btnParams = parsF(styles(btnItem).width) + 'px';
-
-        wave.style.width = btnParams;
-        wave.style.height = btnParams;
-
-        const waveParams = styles(wave);
-
-        wave.style.top = `${e.offsetY - (parsF(waveParams.height) / 2)}px`;
-        wave.style.left = `${e.offsetX - (parsF(waveParams.width) / 2)}px`;
-
-        wave.classList.add('active');
-
-        removingWave('mouseup');
-        removingWave('mouseout');
-
-        function removingWave(event) {
-            btnItem.addEventListener(event, () => {
-            
-                const promise = new Promise((resolve) => {
+    function setWaveToBtn(btn) {
+        btn.addEventListener('mousedown', (e) => {
+            let counter = 0;
     
-                    setTimeout(() => {
-                        wave.style.opacity = '0';
-                        resolve();
-                    }, waveTransition / 1.5);
+            setTimeout(() => {
+                counter = 1;
+            }, waveTransition);
     
-                }).then(() => {
+            const wave = document.createElement('span');
+            wave.classList.add('wave-effect');
+            btn.append(wave);
     
-                    setTimeout(() => {
-                        wave.remove();
-                    }, waveTransition);
+            const btnParams = parsF(styles(btn).width) + 'px';
     
-                });
+            wave.style.width = btnParams;
+            wave.style.height = btnParams;
     
-            }); // mouseup
-        } // removingWave(event)
+            const waveParams = styles(wave);
+    
+            wave.style.top = `${e.offsetY - (parsF(waveParams.height) / 2)}px`;
+            wave.style.left = `${e.offsetX - (parsF(waveParams.width) / 2)}px`;
+    
+            wave.classList.add('active');
+    
+            removingWave('mouseup');
+            removingWave('mouseout');
+    
+            function removingWave(event) {
+                btn.addEventListener(event, () => {
+                
+                    const promise = new Promise((res) => {
+        
+                        if (counter == 1) {
+                            opacityDrop(res);
+                        } else {
+                            setTimeout(() => {
+                                opacityDrop(res);
+                            }, waveTransition / 1.5);
+                        }
+                        
+                    }).then(() => {
+        
+                        setTimeout(() => {
+                            wave.remove();
+                        }, waveTransition);
+        
+                    });
+        
+                }); // mouseup
+            } // removingWave(event)
+    
+            function opacityDrop(res) {
+                wave.style.opacity = '0';
+                res();
+            }
+    
+        }); // mousedown
+    }
 
-    }); // mousedown
+    
 }; // end
 
 
 
 
+/* ========================================================================================== */
 
-
-
-// версия с прошлым вариантом конца анимации
-
-
-    // btnItem.addEventListener('mouseup', () => {
-        
-    //     const promise = new Promise((resolve) => {
-
+    // const btnWave = () => {
+    //     const waveTransition = parsF(styles(qSel('.wave-effect--template')).transitionDuration) * 1000;
+    //     const btnItem = qSel('.btn--transparent');
+    
+    //     btnItem.addEventListener('mousedown', (e) => {
+    //         let counter = 0;
+    
     //         setTimeout(() => {
+    //             counter = 1;
+    //         }, waveTransition);
+    
+    //         const wave = document.createElement('span');
+    //         wave.classList.add('wave-effect');
+    //         btnItem.append(wave);
+    
+    //         const btnParams = parsF(styles(btnItem).width) + 'px';
+    
+    //         wave.style.width = btnParams;
+    //         wave.style.height = btnParams;
+    
+    //         const waveParams = styles(wave);
+    
+    //         wave.style.top = `${e.offsetY - (parsF(waveParams.height) / 2)}px`;
+    //         wave.style.left = `${e.offsetX - (parsF(waveParams.width) / 2)}px`;
+    
+    //         wave.classList.add('active');
+    
+    //         removingWave('mouseup');
+    //         removingWave('mouseout');
+    
+    //         function removingWave(event) {
+    //             btnItem.addEventListener(event, () => {
+                
+    //                 const promise = new Promise((resolve) => {
+        
+    //                     if (counter == 1) {
+    //                         opacityDrop();
+    //                     } else {
+    //                         setTimeout(opacityDrop, waveTransition / 1.5);
+    //                     }
+                        
+    //                 }).then(() => {
+        
+    //                     setTimeout(() => {
+    //                         wave.remove();
+    //                     }, waveTransition);
+        
+    //                 });
+        
+    //             }); // mouseup
+    //         } // removingWave(event)
+    
+    //         function opacityDrop() {
     //             wave.style.opacity = '0';
     //             resolve();
-    //         }, waveTransition / 1.5);
-
-    //     }).then(() => {
-
-    //         setTimeout(() => {
-    //             wave.remove();
-    //         }, waveTransition);
-
-    //     });
-
-    // }); // mouseup
+    //         }
+    
+    //     }); // mousedown
+    // }; // end
