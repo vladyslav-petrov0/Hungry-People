716f3bec8d9008740aebb42cd1123825d97508e6
Vladyslav <m4gdark@gmail.com>
2021-03-20 19:03:45 +0300

    Added some breakpoints

--- assets/js/components/page-scroll.js
+++ assets/js/components/page-scroll.js
@@ -1,71 +1,73 @@
 const pageScroll = () => {
+    if (window.innerWidth > 1024) {
+        const sectionCoords = [];
     
-    const sectionCoords = [];
-    
-    for (let item of qSelA('._scroll-page')) {
-        sectionCoords.push(getCoords(item));
-    }
-    
-    const sectionCoordsLength = sectionCoords.length - 1;
-    let currentPos = 0,
-        counter = 0;
-    
-    for (let item of qSelA('.arrow-scroll')) scrollByButton(item);
-
-    window.addEventListener('wheel', (e) => {
-        if (counter == 0) {
-    
-            reloadCounter();
-    
-            if (e.deltaY > 0) { currentPos++ } 
-            else { currentPos-- };
-    
-            if (currentPos > sectionCoordsLength) {
-                currentPos = sectionCoordsLength;
-            } else if (currentPos < 0) {
-                currentPos = 0;
-            } else {
-                scrollToElem();
-            }
-
+        for (let item of qSelA('._scroll-page')) {
+            sectionCoords.push(getCoords(item));
         }
-    });
+        
+        const sectionCoordsLength = sectionCoords.length - 1;
+        let currentPos = 0,
+            counter = 0;
+        
+        for (let item of qSelA('.arrow-scroll')) scrollByButton(item);
 
-    function scrollByButton(elem) {
-        elem.addEventListener('click', (e) => {
+        window.addEventListener('wheel', (e) => {
             if (counter == 0) {
-
+        
                 reloadCounter();
-
-                if (elem.classList.contains('arrow--down')) { 
-                    currentPos++ 
-                } else { currentPos-- };
-
-                scrollToElem();
+        
+                if (e.deltaY > 0) { currentPos++ } 
+                else { currentPos-- };
+        
+                if (currentPos > sectionCoordsLength) {
+                    currentPos = sectionCoordsLength;
+                } else if (currentPos < 0) {
+                    currentPos = 0;
+                } else {
+                    scrollToElem();
+                }
 
             }
         });
-    }
 
-    function getCoords(el) {
-        const rect = el.getBoundingClientRect();
-    
-        return rect.top + pageYOffset - (window.innerHeight - rect.height);
-    }
+        function scrollByButton(elem) {
+            elem.addEventListener('click', (e) => {
+                if (counter == 0) {
 
-    function reloadCounter() {
-        counter = 1;
-        setTimeout(() => {
-            counter = 0
-        }, 1000)
-    }
+                    reloadCounter();
 
-    function scrollToElem() {
-        scrollTo({
-            top: sectionCoords[currentPos],
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+                    if (elem.classList.contains('arrow--down')) { 
+                        currentPos++ 
+                    } else { currentPos-- };
 
-};
+                    scrollToElem();
+
+                }
+            });
+        }
+
+        function getCoords(el) {
+            const rect = el.getBoundingClientRect();
+        
+            return rect.top + pageYOffset - (window.innerHeight - rect.height);
+        }
+
+        function reloadCounter() {
+            counter = 1;
+            setTimeout(() => {
+                counter = 0
+            }, 1000)
+        }
+
+        function scrollToElem() {
+            scrollTo({
+                top: sectionCoords[currentPos],
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+
+    }  // viewport if
+
+}; // end
