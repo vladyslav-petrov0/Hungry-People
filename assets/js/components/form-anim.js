e1b9a040d09ad6f1a1a20e881f8c897f0eab238f
Vladyslav <m4gdark@gmail.com>
2021-03-26 02:52:49 +0300

    Custom selected was made

--- assets/js/components/form-anim.js
+++ assets/js/components/form-anim.js
@@ -1,19 +1,60 @@
 const formAnim = () => {
-    const formElems = qSelA('.form__elem');
+    const formElems = qSelA('.form--animated');
 
     for (let item of formElems) {
         item.addEventListener('mousedown', () => {
-            const itemNode = qSel('.form__label', item.parentNode);
-            if (!itemNode.classList.contains('chosen')) {
-                itemNode.classList.add('chosen');
+            const nodeLabel = qSel('.form__label', item.parentNode);
+
+            if (!nodeLabel.classList.contains('chosen')) {
+                nodeLabel.classList.add('chosen');
+                item.parentNode.classList.add('selected');
             };
 
-            document.addEventListener('click', function clickOut(e) {
-                if (e.target != item && e.target != itemNode && item.value.length == 0) {
-                    itemNode.classList.remove('chosen');
-                    // document.removeEventListener('click', clickOut);
+            document.addEventListener('mousedown', function clickOut(e) {
+                if (e.target != item && item.value.length == 0) {
+                    nodeLabel.classList.remove('chosen');
+                    item.parentNode.classList.remove('selected');
+
+                    document.removeEventListener('click', clickOut);
                 };
             });
         });
     }
-};
+
+  
+    const selectItems = qSelA('.select');
+
+    for (let item of selectItems) {
+        item.addEventListener('click', (e) => {
+
+            item.classList.toggle('extended');
+            const selectMenu = qSel('.select__menu', item);
+            const menuList = qSelA('li', item);
+
+            if (e.target == item) {
+
+                if (parsF(selectMenu.style.height) > 0) {
+                    selectMenu.style.height = ``;
+                } else {
+                    let menuHeight = 0;
+    
+                    for (let li of menuList) {
+                        menuHeight += parsF(styles(li).height);
+                    }
+        
+                    selectMenu.style.height = `${menuHeight}px`;
+                }
+            }
+
+            for (let li of menuList) {
+                li.addEventListener('click', (e) => {
+                    item.classList.add('selected');
+                    qSel('span', item).innerText = li.innerText;
+                    selectMenu.style.height = ``;
+                });
+            }
+            
+        });
+    } 
+
+}; // end
