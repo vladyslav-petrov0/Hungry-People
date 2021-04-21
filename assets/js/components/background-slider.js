f28a9cc004a1416abe1333b3e77f46de8d90fd6b
Vladyslav <vladyslav.petrov0@gmail.com>
2021-04-21 23:00:08 +0300

    Fixed bug of selecting mannualy slide. Refactored slider

--- assets/js/components/background-slider.js
+++ assets/js/components/background-slider.js
@@ -6,10 +6,10 @@ const backgroundSlider = () => {
         let currentPos = 0;
         let canSlide = 1;
 
-        const sliderBtnsContainer = qSel('.slider-btns', slider.parentNode),
-            sliderBtns = sliderBtnsContainer.children,
+        const btnsContainer = qSel('.slider-btns', slider.parentNode),
+            btns = btnsContainer.children,
             sliderSlides = qSelA('.slider__slide', slider),
-            sliderSlideBackgrounds = [
+            slideBackgrounds = [
                 '/assets/img/chocolate/bg.jpg',
                 '/assets/img/master/bg.jpg',
                 '/assets/img/header/bg.jpg',
@@ -18,29 +18,29 @@ const backgroundSlider = () => {
         const slideAnimDuration = getElemTransition(sliderSlides[0]) * 1000;
 
         for (let i = 0; i < sliderSlides.length; i++) {
-            sliderSlides[i].style.backgroundImage = `url(${sliderSlideBackgrounds[i]})`;
+            sliderSlides[i].style.backgroundImage = `url(${slideBackgrounds[i]})`;
         }
 
         let autoSlideTimer = setInterval(autoSlide, 5000);
-        sliderBtnsContainer.addEventListener('click', selectSlide);
+        btnsContainer.addEventListener('click', selectSlide);
 
         function selectSlide(e) {
             if (e.target.tagName == 'BUTTON' && canSlide) {
-                currentPos = Array.from(sliderBtns).findIndex(item => item == e.target);
+                currentPos = Array.from(btns).findIndex(item => item == e.target);
 
                 const secondSlide = qSel('.slider__slide--second', slider);
-                secondSlide.style.backgroundImage = `url(${sliderSlideBackgrounds[currentPos]})`;
+                secondSlide.style.backgroundImage = `url(${slideBackgrounds[currentPos]})`;
                 nextSlide();
                 holdAutoSlide();
             }
         }
 
         function switchCurrentBtn() {
-            for (let btn of sliderBtns) {
+            for (let btn of btns) {
                 btn.classList.remove('slider__btn--active');
             }
 
-            sliderBtns[currentPos].classList.add('slider__btn--active');
+            btns[currentPos].classList.add('slider__btn--active');
         }
 
         function autoSlide() {
@@ -51,7 +51,7 @@ const backgroundSlider = () => {
 
             function changeCurrentPos() {
                 currentPos++;
-                if (currentPos == sliderSlideBackgrounds.length) {
+                if (currentPos == slideBackgrounds.length) {
                     currentPos = 0;
                 }
             }
@@ -63,12 +63,7 @@ const backgroundSlider = () => {
                 secondSlide = qSel('.slider__slide--second', slider),
                 slideIsFirst = currentSlide == currentSlide.parentNode.children[0];
 
-            if (slideIsFirst) {
-                currentSlide.classList.add('slider__slide--clipped-reverse');
-            } else {
-                currentSlide.classList.add('slider__slide--clipped');
-            }
-
+            clipCurrentSlide();
             disableRandomSlide();
             switchCurrentBtn();
 
@@ -87,16 +82,27 @@ const backgroundSlider = () => {
 
                 secondSlide.classList.add('slider__slide--current');
                 secondSlide.classList.remove('slider__slide--second');
-                
+      
                 function getAvailableSlideBackground() {
-                    const usedSlideBackgrounds = Array.from(qSelA('.slider__slide', slider))
-                    .map(item => styles(item).backgroundImage
-                    .match(/(\/assets\/img\/).+(\.jpg)|(\.png)/i)[0]);
-                
-                    return sliderSlideBackgrounds.find((item) => !usedSlideBackgrounds.includes(item));
+                    const secondSlideImageUrl = secondSlide.style.backgroundImage.match(/(\/assets\/img\/).+(\.jpg)|(\.png)/i)[0];
+                    const secondSlideImageUrlIndex = slideBackgrounds.indexOf(secondSlideImageUrl);
+
+                    if (secondSlideImageUrlIndex == slideBackgrounds.length - 1) {
+                        return slideBackgrounds[0];
+                    } else {
+                        return slideBackgrounds[secondSlideImageUrlIndex + 1];
+                    }
                 }
-                
+
             }, slideAnimDuration);
+
+            function clipCurrentSlide() {
+                if (slideIsFirst) {
+                    currentSlide.classList.add('slider__slide--clipped-reverse');
+                } else {
+                    currentSlide.classList.add('slider__slide--clipped');
+                }
+            }
 
         } //nextslide
         
@@ -119,9 +125,3 @@ const backgroundSlider = () => {
     } // for loop
 
 };
-
-/*
-неккоректно работает функция первого доступного слайда,
-при попытке ручного выбора слайда, например, когда текущий слайд
-является следующим после того, который мы хотим выбрать
-*/
